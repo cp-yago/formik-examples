c24aa1ab402d96132fb8cbc76281395f86e97083
Yago Cunha <yago.cunha123@gmail.com>
2020-10-04 11:27:01 -0300

    finishing example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from "yup";
 
 function App() {
+  const onSubmit = (values, actions) => {
+    console.log('SUBMIT: ', values);
+  }
+
+  const schema = Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().email().required()
+  }); 
+
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Formik
+        validationSchema={schema}
+        validateOnMount
+        onSubmit={onSubmit} 
+        initialValues={{
+          name: '',
+          email: '',
+        }}
+        render={({ values, handleSubmit, errors, touched, isValid }) => (
+          <Form>
+            <div>
+              <Field name="name" type="text" />
+              <ErrorMessage name="name"/>
+              <Field name="email" type="text" />
+              <ErrorMessage name="email"/>
+            </div>
+            <button type="submit" disabled={!isValid}>Enviar</button>
+          </Form>
+        )}
+      />
     </div>
   );
 }
